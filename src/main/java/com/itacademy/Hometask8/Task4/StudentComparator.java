03d2108e1f6ad5be9cdeaf9b78a23247568b7541
KseniyaFilonenko <filonenko_k@tut.by>
2024-04-12 14:56:10 +0300

    Hometask8 updated

--- src/main/java/com/itacademy/Hometask8/Task4/StudentComparator.java
+++ src/main/java/com/itacademy/Hometask8/Task4/StudentComparator.java
@@ -1,2 +1,10 @@
-package com.itacademy.Hometask8.Task4;public class StudentComparator {
+package com.itacademy.Hometask8.Task4;
+
+import java.util.Comparator;
+
+public class StudentComparator implements Comparator<Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        return o1.age - o2.age;
+    }
 }
