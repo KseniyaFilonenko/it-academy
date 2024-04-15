2b1971091735429eada3fbf8a6fbc7a5454d3c81
KseniyaFilonenko <filonenko_k@tut.by>
2024-04-15 16:11:10 +0300

    Hometask9 is added

---
+++ src/main/java/com/itacademy/Hometask9/FuncInterface.java
@@ -0,0 +1,6 @@
+package com.itacademy.Hometask9;
+
+@FunctionalInterface
+public interface FuncInterface <T> {
+    T doSmth (T t);
+}
--- src/test/java/com/itacademy/AppTest.java
+++ src/test/java/com/itacademy/AppTest.java
@@ -5,15 +5,15 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit text2 for simple App.
  */
 public class AppTest 
     extends TestCase
 {
     /**
-     * Create the test case
+     * Create the text2 case
      *
-     * @param testName name of the test case
+     * @param testName name of the text2 case
      */
     public AppTest( String testName )
     {
